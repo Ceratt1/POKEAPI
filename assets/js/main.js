63f9fac19d25b0d416efc37b55abcf031aff1262
Gabriel Ceratti Cabral <gabrielceratticabral@gmail.com>
2024-03-25 20:35:10 -0300

    api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-alert('hey');
+let offset = 0;
+let limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+
+fetch(url).then(function (response) {
+    console.log(response.status);
+})
+
